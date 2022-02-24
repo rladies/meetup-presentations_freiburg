8e6c07035ce4dc43ae70a2a540b8308cf3fa8084
kyla-mcconnell <kylamcconnell@live.com>
2022-02-24 19:03:09 +0100

    Update app final

--- 2022-02-24_Shiny_Intro/demo_app/app.R
+++ 2022-02-24_Shiny_Intro/demo_app/app.R
@@ -3,15 +3,20 @@ library(shiny)
 
 # ------------------- User interface
 ui <- fluidPage(
-    selectInput(inputId = "greeting", label = "Who do you want to greet?", 
-                choices = c("world" = "Hello, world!", "friends" = "Hi, friends!", 
-                            "people" = "Hey, people!")),
-    textOutput("full_greeting")
+    sliderInput(inputId = "age", label = "How old are you?", min = 1, max = 80, value = 6),
+    textOutput("age_greeting")
 )
 
 # ------------------- Server
 server <- function(input, output, session) {
-    output$full_greeting <- renderText(input$greeting)
+    output$age_greeting <- renderText({
+        dog_age <- input$age * 7,
+        fish_age <- input$age * 45
+        age_greeting <- paste("That's ", dog_age, " in dog years and ", fish_age, " in fish years!")
+    })
 }
 
 shinyApp(ui, server)
+
+
+
